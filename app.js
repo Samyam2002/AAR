3c7a0e794edd90668e875c38cfb42f0bb8d985c0
Samyam2002 <ssamyam20@tbc.edu.np>
2023-04-28 02:07:10 +0545

    new

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as THREE from './libs/three125/three.module.js';
-import { GLTFLoader } from '/libs/three/jsm/GLTFLoader.js';
+import { GLTFLoader } from './libs/three/jsm/GLTFLoader.js';
 import { RGBELoader } from './libs/three/jsm/RGBELoader.js';
 import { ARButton } from './libs/ARButton.js';
 import { LoadingBar } from './libs/LoadingBar.js';
